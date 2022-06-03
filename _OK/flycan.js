01b0ef2e084434504672919cad21c58ade5bf66d
Jack123447 <65748971+Jack123447@users.noreply.github.com>
2022-06-03 23:54:18 +0800

    Initial commit

---
+++ _OK/flycan.js
@@ -0,0 +1,11 @@
+$("#btn1").click(function() {
+    $("#pic").show(300);
+});
+
+$("#btn2").click(function() {
+    $("#pic").hide(300);
+});
+
+$("#btn3").click(function() {
+    $("#pic").toggle(300);
+});
---
+++ flycan.js
@@ -0,0 +1,14 @@
+$(".btn1").click(function(){
+
+	$("img").show(500);
+})
+
+$(".btn2").click(function(){
+
+	$("img").hide(500);
+})
+
+$(".btn3").click(function(){
+
+	$("img").toggle(500);
+})
